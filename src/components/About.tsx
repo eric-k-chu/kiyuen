58cb59a698b69a386235d61ced30eb1c61c5b738
eric-k-chu <kiyuen88ec@gmail.com>
2024-01-29 15:59:08 -0800

    initial carousel design

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ export function About() {
         <h1 className="text-5xl font-semibold">About</h1>
         <p className="max-w-[500px] py-4 text-lg">
           Building is something I've always been passionate about. From building
-          Legos to theoryCrafting a Path of Exile build to designing a web app,
+          Legos to theory-crafting a Path of Exile build to designing a web app,
           it's been my primary drive for becoming a software developer.
         </p>
       </div>
