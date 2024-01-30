dbb45afa8dd8b9fdee8c2f077ca73d0366c038fa
eric-k-chu <kiyuen88ec@gmail.com>
2024-01-29 16:09:05 -0800

    changed layout for carousel

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -20,10 +20,18 @@ export function Projects() {
       <h1 className="text-5xl font-semibold">Projects</h1>
       <div className="flex items-center justify-between py-4">
         <button onClick={prev}>PREV</button>
-        <div className="relative flex h-[400px] w-[500px] items-center justify-center bg-red-400">
-          <h1>{arr[selected]}</h1>
-          <div className="absolute left-40 z-[-1] h-[400px] w-[500px] -scale-90 bg-blue-400/50" />
-          <div className="absolute right-40 z-[-1] h-[400px] w-[500px] -scale-90 bg-blue-400/50" />
+        <div className="relative h-[400px] w-[1000px]">
+          <div className=" absolute z-[2] flex h-[400px] w-[500px] translate-x-[50%] items-center justify-center bg-red-400">
+            <h1>{arr[selected]}</h1>
+          </div>
+          <div
+            className="absolute z-[1] h-[400px] w-[500px] translate-x-[25%] -scale-90 bg-green-400/50 hover:cursor-pointer"
+            onClick={prev}
+          />
+          <div
+            className="absolute z-[1] h-[400px] w-[500px] translate-x-[75%] -scale-90 bg-blue-400/50 hover:cursor-pointer"
+            onClick={next}
+          />
         </div>
         <button onClick={next}>NEXT</button>
       </div>
