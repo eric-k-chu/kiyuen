4a5cb99fc9a253daa70bb1f22cdc21f475414c91
Eric Chu <Kiyuen88ec@gmail.com>
2024-01-30 21:57:24 -0800

    changed order of projects

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,10 +8,26 @@ interface Project {
 
 export const PROJECTS: Project[] = [
   {
-    name: "Project S",
-    path: "/image/projects.png",
-    desc: "A 2.5D action platformer",
-    gh: "https://github.com/eric-k-chu/Project-S",
+    name: "Castle v2",
+    path: "/image/castle2.png",
+    desc: "A Chess.com statistics site with Next.js",
+    gh: "https://github.com/eric-k-chu/Castle-v2",
+    live: "https://castle2.vercel.app/",
+  },
+  {
+    name: "Artus",
+    path: "/image/artus.png",
+    desc: "A full-stack video hosting web app.",
+    gh: "https://github.com/eric-k-chu/Artus",
+    live: "http://artus-dev.us-west-1.elasticbeanstalk.com/",
+  },
+
+  {
+    name: "Castle",
+    path: "/image/castle.png",
+    desc: "A Chess.com statistics site.",
+    gh: "https://github.com/eric-k-chu/Castle",
+    live: "https://eric-k-chu.github.io/Castle/",
   },
   {
     name: "Geo Run",
@@ -21,24 +37,9 @@ export const PROJECTS: Project[] = [
     live: "https://eric-k-chu.github.io/Geo-Run-Host/",
   },
   {
-    name: "Castle",
-    path: "/image/castle.png",
-    desc: "A Chess.com statistics site.",
-    gh: "https://github.com/eric-k-chu/Castle",
-    live: "https://eric-k-chu.github.io/Castle/",
-  },
-  {
-    name: "Artus",
-    path: "/image/artus.png",
-    desc: "A full-stack video hosting web app.",
-    gh: "https://github.com/eric-k-chu/Artus",
-    live: "http://artus-dev.us-west-1.elasticbeanstalk.com/",
-  },
-  {
-    name: "Castle v2",
-    path: "/image/castle2.png",
-    desc: "A Chess.com statistics site with Next.js",
-    gh: "https://github.com/eric-k-chu/Castle-v2",
-    live: "https://castle2.vercel.app/",
+    name: "Project S",
+    path: "/image/projects.png",
+    desc: "A 2.5D action platformer",
+    gh: "https://github.com/eric-k-chu/Project-S",
   },
 ];
