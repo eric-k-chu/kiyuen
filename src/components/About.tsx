0b7eafa6c496f589850260fe8bb97ef464010d2c
Eric Chu <Kiyuen88ec@gmail.com>
2024-01-31 20:56:33 -0800

    fixed responsiveness of about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,13 @@
 export function About() {
   return (
-    <div className="flex justify-between">
+    <div className="flex flex-wrap justify-between">
       <div>
         <h1 className="text-5xl font-semibold">About</h1>
         <p className="max-w-[500px] py-4 text-lg">
-          Building is something I've always been passionate about. From building
-          Legos to theory-crafting a Path of Exile build to designing a web app,
-          it's been my primary drive for becoming a software developer.
+          Building is something I&#39;ve always been passionate about&#46; From
+          building Legos to theory-crafting a Path of Exile build to designing a
+          web app&#44; it&#39;s been my primary drive for becoming a software
+          developer&#46;
         </p>
       </div>
       <div className="h-[300px] w-[400px] rounded-md bg-red-400" />
