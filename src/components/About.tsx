839a06970fd29460c36f4e802c518b47e7d49976
Eric Chu <Kiyuen88ec@gmail.com>
2024-02-01 16:36:38 -0800

    updated about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,33 +4,35 @@ export function About() {
   return (
     <>
       <h1 className="text-5xl font-semibold uppercase">About</h1>
-      <p className="my-4 max-w-[500px] font-sans text-lg">
-        Building is something I&#39;ve always been passionate about&#46; From
-        building Legos to theory-crafting a Path of Exile build to designing a
-        web app&#44; it&#39;s been my primary drive for becoming a software
-        developer&#46;
-      </p>
+      <div className="space-y-8">
+        <p className="my-4 font-sans text-lg sm:text-xl md:text-2xl">
+          Building is something I&#39;ve always been passionate about&#46; From
+          building Legos to theory-crafting a Path of Exile build to designing a
+          web app&#44; it&#39;s been my primary drive for becoming a software
+          developer&#46;
+        </p>
+        <div className="flex w-full items-center justify-around">
+          <Interests name="Web Development" path="/image/web.svg" />
+          <Interests name="Game Development" path="/image/controller.svg" />
+        </div>
+      </div>
     </>
   );
 }
 
-function tes() {
+function Interests({ name, path }: { name: string; path: string }) {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <Image
-        src="/image/web.svg"
-        alt="web development"
+        src={path}
+        alt={name}
         width={0}
         height={0}
-        className="h-40 w-auto"
+        className="h-16 w-auto sm:h-24 md:h-32 lg:h-40"
       />
-      <Image
-        src="/image/controller.svg"
-        alt="game development"
-        width={0}
-        height={0}
-        className="h-40 w-auto"
-      />
+      <h2 className="text-base font-semibold sm:text-lg md:text-xl lg:text-2xl">
+        {name}
+      </h2>
     </div>
   );
 }
