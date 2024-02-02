cc772c0f427cc71467bfa7b61b56d19630c23aef
Eric Chu <Kiyuen88ec@gmail.com>
2024-02-01 17:04:11 -0800

    Added separator

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,23 +2,27 @@
 
 // must have use client if importing an object with a JSX property
 import { PAGE_LINKS } from "@/lib/sections";
-import { useState } from "react";
+import { useState, Fragment } from "react";
 
 export function Navbar() {
   return (
     <nav className="fixed top-0 z-10 flex w-full items-center justify-between bg-zinc-900/90 px-12 py-6 backdrop-blur-sm">
       <h1 className="text-xl font-medium">朱</h1>
       <MobileMenu />
-      <nav className="hidden items-center gap-x-8 text-xl font-semibold tracking-wider md:flex">
-        {PAGE_LINKS.map((n) => (
-          <a
-            href={`#${n.name}`}
-            key={n.name}
-            rel="noopener"
-            className="border-b-2 border-b-transparent uppercase transition-colors duration-150 ease-in-out hover:border-b-primary-blue"
-          >
-            {n.name}
-          </a>
+      <nav className="hidden items-center gap-x-4 text-xl font-semibold tracking-wider md:flex">
+        {PAGE_LINKS.map((n, i) => (
+          <Fragment key={n.name}>
+            <a
+              href={`#${n.name}`}
+              rel="noopener"
+              className="border-b-2 border-b-transparent uppercase transition-colors duration-150 ease-in-out hover:border-b-primary-blue"
+            >
+              {n.name}
+            </a>
+            {i < PAGE_LINKS.length - 1 && (
+              <span className="font-medium">&#47;</span>
+            )}
+          </Fragment>
         ))}
       </nav>
     </nav>
