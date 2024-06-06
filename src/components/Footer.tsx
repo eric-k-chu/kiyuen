7e9f870fb619d00100f7142b031d58e7db14b19f
Eric Chu <Kiyuen88ec@gmail.com>
2024-06-05 21:57:26 -0700

    added updated to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,15 @@ import { GitHubLink, LinkedInLink } from './Socials'
 
 export function Footer(): React.ReactElement {
   return (
-    <footer className='grid min-h-64 w-full grid-cols-2 items-center bg-black p-6'>
+    <footer className='relative grid min-h-64 w-full grid-cols-2 items-center bg-black p-6'>
       <strong className='text-center text-xl text-white'>Eric Chu</strong>
       <div className='flex items-center justify-center gap-x-4'>
         <GitHubLink className='invert' />
         <LinkedInLink />
       </div>
+      <p className='absolute bottom-4 w-full text-center text-xs text-muted-foreground'>
+        Updated 6/5/2024
+      </p>
     </footer>
   )
 }
