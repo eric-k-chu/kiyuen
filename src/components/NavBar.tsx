51311eb23cf6e5b81d179b37d711b426fefd08f0
Eric Chu <Kiyuen88ec@gmail.com>
2024-06-05 22:06:24 -0700

    Added logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,19 @@ import { LINKS } from '@/common'
 import { NavDrawer } from './NavDrawer'
 import { NavLink } from './NavLink'
 import { ThemeToggle } from './ThemeToggle'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui'
 
 export function NavBar(): React.ReactElement {
   return (
     <div className='fixed top-0 z-10 flex w-screen items-center justify-between border-b bg-background px-4 py-1'>
-      <h1 className='text-red-400'>TODO LOGO</h1>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <strong className='cursor-default select-none text-xl'>朱</strong>
+          </TooltipTrigger>
+          <TooltipContent>Chu</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
       <nav className='hidden items-center gap-x-4 sm:flex'>
         {LINKS.map((link) => (
           <NavLink key={link} link={link} />
