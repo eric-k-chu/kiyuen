c06d057454ddba9d650c89586fbb660ebf9cbb70
Eric Chu <Kiyuen88ec@gmail.com>
2024-06-05 21:54:03 -0700

    wider nav link

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -8,7 +8,10 @@ export type NavLinkProps = {
 
 export function NavLink({ link }: NavLinkProps): React.ReactElement {
   return (
-    <a href={toId(link)} className='inline-flex flex-col items-center uppercase hover:no-underline'>
+    <a
+      href={toId(link)}
+      className='inline-flex flex-col items-center uppercase tracking-wider hover:no-underline'
+    >
       {link}
     </a>
   )
