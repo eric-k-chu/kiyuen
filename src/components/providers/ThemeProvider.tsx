99298ed283dc175b008faf11cf9f62faeca551ec
eric-k-chu <kiyuen88ec@gmail.com>
2024-06-09 07:53:50 -0700

    default light theme

--- src/components/providers/ThemeProvider.tsx
+++ src/components/providers/ThemeProvider.tsx
@@ -4,5 +4,9 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { PropsWithChildren } from 'react'
 
 export function ThemeProvider({ children }: PropsWithChildren): React.ReactElement {
-  return <NextThemesProvider attribute='class'>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider attribute='class' defaultTheme='light'>
+      {children}
+    </NextThemesProvider>
+  )
 }
