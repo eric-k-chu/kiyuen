a137cb52bb95be3054fcd7a7931cdb6bc86fe2b6
Eric Chu <Kiyuen88ec@gmail.com>
2024-06-15 20:50:42 -0700

    bg change

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -6,11 +6,18 @@ type Props = PropsWithChildren<{
   className?: string
 }>
 
+/**
+cn(
+        'flex w-full min-w-0 shrink-0 grow-0 basis-full items-center justify-center py-4 first:bg-gradient-to-t first:from-background first:from-10% first:to-transparent [&:not(:first-child)]:odd:bg-gradient-to-b [&:not(:first-child)]:odd:from-background [&:not(:first-child)]:odd:via-background/40 [&:not(:first-child)]:odd:via-30% [&:not(:first-child)]:odd:to-background [&:not(:first-child)]:odd:to-90% [&:not(:first-child)]:even:bg-background',
+        className
+      )
+ */
+
 export function SectionContainer({ children, id, className }: Props): ReactElement {
   return (
     <section
       className={cn(
-        'flex w-full min-w-0 shrink-0 grow-0 basis-full items-center justify-center py-4 first:bg-gradient-to-t first:from-background first:from-10% first:to-transparent [&:not(:first-child)]:odd:bg-gradient-to-b [&:not(:first-child)]:odd:from-background [&:not(:first-child)]:odd:via-background/40 [&:not(:first-child)]:odd:via-30% [&:not(:first-child)]:odd:to-background [&:not(:first-child)]:odd:to-90% [&:not(:first-child)]:even:bg-background',
+        'flex w-full min-w-0 shrink-0 grow-0 basis-full items-center justify-center py-4 odd:bg-background/60 even:bg-background',
         className
       )}
       id={id}
