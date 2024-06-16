2f9a731f6470a0b30eca4df9c56a6039a2f7e3cd
Eric Chu <Kiyuen88ec@gmail.com>
2024-06-16 14:56:49 -0700

    nav link hover color

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -9,12 +9,12 @@ export function NavLink({ link }: NavLinkProps): ReactElement {
   return (
     <a
       href={`#${link.toLowerCase()}`}
-      className='group text-muted-foreground transition-colors ease-in-out hover:text-foreground'
+      className='group text-foreground transition-colors ease-in-out hover:text-primary'
     >
       {link}
       <span
         aria-hidden
-        className='block h-0.5 w-full max-w-0 bg-foreground transition-all ease-in-out group-hover:max-w-full'
+        className='block h-0.5 w-full max-w-0 bg-primary transition-all ease-in-out group-hover:max-w-full'
       />
     </a>
   )
