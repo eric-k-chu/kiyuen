8764cf6f3f55de4ea2348c2f630c988b95f2ea51
eric-k-chu <kiyuen88ec@gmail.com>
2024-11-18 00:14:40 -0800

    feat: react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import path from 'node:path'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', { target: '18' }]],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
