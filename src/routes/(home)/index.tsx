8e1f24c3fcb5be5fbe5bcf7cdf91a4caa683441e
eric-k-chu <kiyuen88ec@gmail.com>
2024-11-28 12:23:32 -0800

    fix format

--- src/routes/(home)/index.tsx
+++ src/routes/(home)/index.tsx
@@ -12,8 +12,8 @@ export const Route = createFileRoute('/(home)/')({
 function Home(): ReactElement {
   return (
     <CarouselProvider>
-      <div className="flex min-h-screen items-center justify-center">
-        <section className="flex h-96 justify-center gap-x-3">
+      <div className='flex min-h-screen items-center justify-center'>
+        <section className='flex h-96 justify-center gap-x-3'>
           <Profile />
           <Resume />
           <Menubar />
