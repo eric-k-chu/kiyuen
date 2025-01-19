77b5fded6529b05e23a034bd316f5d90e859a264
eric-k-chu <kiyuen88ec@gmail.com>
2025-01-18 21:34:17 -0800

    wip

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,15 +1,25 @@
+import { chunk } from '@/lib'
 import { readBlogs } from '@/slib'
+import Link from 'next/link'
 import type { ReactElement } from 'react'
 
 export default async function Blogs(): Promise<ReactElement> {
   const blogs = await readBlogs()
+  const chunks = chunk(blogs, 3)
   return (
-    <div className='flex h-dvh items-center justify-center'>
-      {blogs.map((blog) => (
-        <div key={blog.title}>
-          <h1>{blog.title}</h1>
-          <p>{blog.description}</p>
-          <p>{blog.date.toDateString()}</p>
+    <div className='mx-auto grid h-dvh max-w-2xl grid-cols-2 gap-4 md:grid-cols-4'>
+      {chunks.map((blogs, i) => (
+        <div className='grid gap-4' key={i}>
+          {blogs.map((blog) => (
+            <Link
+              key={blog.title}
+              href={`/blogs/${blog.title}`}
+              className='rounded-md border border-ctp-rosewater p-4 shadow'
+            >
+              <h1 className='font-bold text-xl'>{blog.title}</h1>
+              <p className='text-gray-600 text-sm'>{blog.description}</p>
+            </Link>
+          ))}
         </div>
       ))}
     </div>
