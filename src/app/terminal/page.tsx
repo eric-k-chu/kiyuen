2134218aa566f841774fd93d70b7fafeacdc273e
Eric Chu <Kiyuen88ec@gmail.com>
2025-02-23 21:39:13 -0800

    fix(terminal): use navigation

--- src/app/terminal/page.tsx
+++ src/app/terminal/page.tsx
@@ -1,15 +1,11 @@
-import { CornerDownRight } from 'lucide-react'
-import Link from 'next/link'
+import { Navigation } from '@/components'
 import type { ReactElement } from 'react'
 
 export default function Terminal(): ReactElement {
   return (
     <main className='flex h-dvh flex-col items-center justify-center gap-2 px-16'>
       <h2 className='text-lg uppercase tracking-widest sm:text-2xl'>In Development</h2>
-      <Link href='/' className='px-4 py-2 text-ctp-text'>
-        <CornerDownRight className='mr-2 inline size-4' />
-        Home
-      </Link>
+      <Navigation className='space-x-4' />
     </main>
   )
 }
