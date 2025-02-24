fed3ea4a26a6ca6f3771816706c9e451e9baad80
Eric Chu <Kiyuen88ec@gmail.com>
2025-02-23 21:45:28 -0800

    fix(terminal): exclude terminal from nav tray

--- src/app/terminal/page.tsx
+++ src/app/terminal/page.tsx
@@ -5,7 +5,7 @@ export default function Terminal(): ReactElement {
   return (
     <main className='flex h-dvh flex-col items-center justify-center gap-2 px-16'>
       <h2 className='text-lg uppercase tracking-widest sm:text-2xl'>In Development</h2>
-      <Navigation className='space-x-4' />
+      <Navigation className='space-x-4' hide={['terminal']} />
     </main>
   )
 }
