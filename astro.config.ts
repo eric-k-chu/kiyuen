ea25652545d65b954f31fb557e31bf1a1f6041fc
Eric Chu <Kiyuen88ec@gmail.com>
2025-06-07 22:37:10 -0700

    chore: remove mdx

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,10 @@
-import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://www.kiyuen.dev',
-  integrations: [mdx(), sitemap()],
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
